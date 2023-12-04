c7f058b67e108fc7f5dc9767609c19e36b1d970e
Per Rovegård <per@rovegard.se>
2023-12-04 07:56:40 +0100

    Day 4 part 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 use util::Input;
 
@@ -38,7 +38,27 @@ fn part1(input: &Input) -> Result<u32> {
 }
 
 fn part2(input: &Input) -> Result<u32> {
-    Ok(0)
+    let mut card_counts = HashMap::<u32, u32>::new();
+
+    let cards: Vec<_> = input.as_lines().map(|line| to_card(line)).collect();
+
+    // Set initial counts
+    for card in &cards {
+        card_counts.insert(card.id, 1);
+    }
+
+    for card in &cards {
+        let winning_card_count = card_counts.get(&card.id).unwrap().clone();
+        let win_count = card.on_hand.iter().fold(0u32, |acc, num| if card.winning.contains(num) { acc + 1 } else { acc });
+        let next = card.id + 1;
+        for following_card_id in next..(next + win_count) {
+            let following_card_count = card_counts.get(&following_card_id).unwrap();
+            card_counts.insert(following_card_id, following_card_count + winning_card_count);
+        }
+    }
+
+    let result = card_counts.values().sum();
+    Ok(result)
 }
 
 #[cfg(test)]
@@ -68,11 +88,24 @@ mod test {
         Ok(())
     }
 
-    // #[test]
-    // pub fn test_part2() -> Result<()> {
-    //     let input = Input::from_lines([
-    //     ]);
-    //     assert_eq!(part2(&input).unwrap(), 0);
-    //     Ok(())
-    // }
+    #[test]
+    pub fn test_part2() -> Result<()> {
+        let input = Input::from_lines([
+            "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+            "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+            "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+            "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+            "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+            "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+        ]);
+        assert_eq!(part2(&input).unwrap(), 30);
+        Ok(())
+    }
+
+    #[test]
+    pub fn test_part2_input() -> Result<()> {
+        let input = Input::load("input")?;
+        assert_eq!(part2(&input).unwrap(), 5329815);
+        Ok(())
+    }
 }
