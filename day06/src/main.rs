5ce52b0fcfe8201540efd2e12610bed81325fa20
Per Rovegård <per@rovegard.se>
2023-12-06 07:37:49 +0100

    Day 6, part 2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -4,23 +4,23 @@ use itertools::Itertools;
 use util::Input;
 
 struct Race {
-    time: u32,
-    distance: u32,
+    time: u64,
+    distance: u64,
 }
 
-fn extract_numbers(s: &str) -> Vec<u32> {
+fn extract_numbers(s: &str) -> Vec<u64> {
     s
         .find(":")
         .iter()
         .flat_map(|col_idx| s.get((col_idx+1)..))
         .flat_map(|ns| ns.trim().split_ascii_whitespace())
-        .map(|ns| ns.parse::<u32>().unwrap())
+        .map(|ns| ns.parse::<u64>().unwrap())
         .collect_vec()
 }
 
 impl Race {
-    fn from_input(input: &Input) -> Vec<Race> {
-        let mut iter = input.as_lines();
+    fn from_lines(lines: Vec<String>) -> Vec<Race> {
+        let mut iter = lines.iter();
         let first_opt = iter.next();
         let second_opt = iter.next();
 
@@ -36,16 +36,34 @@ impl Race {
         }
     }
 
-    fn distance_from_hold_time(&self, ht: u32) -> u32 {
+    fn from_input(input: &Input) -> Vec<Race> {
+        Race::from_lines(input.as_lines().map(|s| s.to_string()).collect_vec())
+    }
+
+    fn from_input_p2(input: &Input) -> Vec<Race> {
+        let lines = input.as_lines()
+            .map(|line| {
+                let no_ws: String = line.chars().filter(|c| !c.is_whitespace()).collect();
+                no_ws
+            })
+            .collect_vec();
+        Race::from_lines(lines)
+    }
+
+    fn distance_from_hold_time(&self, ht: u64) -> u64 {
         if ht >= self.time { 0 } else { (self.time - ht) * ht }
     }
 
-    fn is_win(&self, distance: u32) -> bool {
+    fn is_win(&self, distance: u64) -> bool {
         distance > self.distance
     }
 
-    fn win_times(&self) -> Vec<u32> {
+    fn win_times(&self) -> Vec<u64> {
         (0..self.time).filter(|ht| self.is_win(self.distance_from_hold_time(*ht))).collect_vec()
+    }
+
+    fn win_count(&self) -> usize {
+        (0..self.time).filter(|ht| self.is_win(self.distance_from_hold_time(*ht))).count()
     }
 
 }
@@ -59,7 +77,12 @@ fn part1(input: &Input) -> Result<u32> {
 }
 
 fn part2(input: &Input) -> Result<u32> {
-    Ok(0)
+    let races = Race::from_input_p2(input);
+    let result = match races.first() {
+        Some(r) => r.win_count() as u32,
+        None => panic!("No first race")
+    };
+    Ok(result)
 }
 
 #[cfg(test)]
@@ -85,11 +108,20 @@ mod test {
         Ok(())
     }
 
-    // #[test]
-    // pub fn test_part2() -> Result<()> {
-    //     let input = Input::from_lines([
-    //     ]);
-    //     assert_eq!(part2(&input).unwrap(), 0);
-    //     Ok(())
-    // }
+    #[test]
+    pub fn test_part2() -> Result<()> {
+        let input = Input::from_lines([
+            "Time:      7  15   30",
+            "Distance:  9  40  200",
+        ]);
+        assert_eq!(part2(&input).unwrap(), 71503);
+        Ok(())
+    }
+
+    #[test]
+    pub fn test_part2_input() -> Result<()> {
+        let input = Input::load("input")?;
+        assert_eq!(part2(&input).unwrap(), 23632299);
+        Ok(())
+    }
 }
