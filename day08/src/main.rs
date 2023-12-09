15736292d4867f802ca6fa0d6e59597025746875
Per Rovegård <per@rovegard.se>
2023-12-09 14:05:49 +0100

    Day 8, part 2

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,10 +1,13 @@
+use std::collections::HashMap;
+
 use anyhow::Result;
 use itertools::Itertools;
 use regex::Regex;
 
 use util::Input;
+use num_integer::lcm;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Node {
     name: String,
     left: String,
@@ -14,14 +17,14 @@ struct Node {
 #[derive(Debug)]
 struct Map {
     instructions: Vec<char>,
-    nodes: Vec<Node>,
+    nodes: HashMap<String, Node>,
 }
 
 fn parse_input(input: &Input) -> Map {
     let lines = input.as_lines().collect_vec();
     let instructions = lines.first().unwrap().chars().collect_vec();
 
-    let re = Regex::new(r"(?<node>[A-Z]{3}) = \((?<left>[A-Z]{3}), (?<right>[A-Z]{3})\)").unwrap();
+    let re = Regex::new(r"(?<node>[A-Z0-9]{3}) = \((?<left>[A-Z0-9]{3}), (?<right>[A-Z0-9]{3})\)").unwrap();
 
     let nodes = lines.iter().skip(2).map(|line| {
         match re.captures(line) {
@@ -35,7 +38,9 @@ fn parse_input(input: &Input) -> Map {
         }
     }).collect_vec();
 
-    Map { instructions, nodes }
+    let hm: HashMap<String, Node> = nodes.iter().map(|n| (n.name.to_string(), n.clone())).collect();
+
+    Map { instructions, nodes: hm }
 }
 
 fn steps(map: &Map) -> u32 {
@@ -46,7 +51,7 @@ fn steps(map: &Map) -> u32 {
 
     while node_name != "ZZZ" {
         let lr = map.instructions.get(idx).unwrap();
-        let node = map.nodes.iter().find(|n| n.name == node_name).unwrap();
+        let node = map.nodes.get(node_name).unwrap();
     
         node_name = if *lr == 'L' { &node.left } else { &node.right };
 
@@ -57,14 +62,43 @@ fn steps(map: &Map) -> u32 {
     step_count
 }
 
+fn find_period(map: &Map, initial_node_name: &str) -> u32 {
+    // Mutable solution to avoid stack overflow. Is there a way to force tail recursion?
+    let mut node_name = initial_node_name;
+    let mut idx = 0;
+    let mut step_count = 0;
+
+    while !node_name.ends_with("Z") {
+        let lr = map.instructions.get(idx).unwrap();
+        let node = map.nodes.get(node_name).unwrap();
+    
+        node_name = if *lr == 'L' { &node.left } else { &node.right };
+
+        idx = (idx + 1) % map.instructions.len();
+        step_count += 1;
+    }
+
+    step_count
+}
+
+fn steps_sim(map: &Map) -> u64 {
+    let node_names = map.nodes.keys().filter(|k| k.ends_with("A")).collect_vec();
+
+    let periods = node_names.iter().map(|nn| find_period(map, nn)).collect_vec();
+
+    periods.iter().fold(1u64, |acc, p| lcm(acc, *p as u64))
+}
+
 fn part1(input: &Input) -> Result<u32> {
     let map = parse_input(input);
     let result = steps(&map);
     Ok(result)
 }
 
-fn part2(input: &Input) -> Result<u32> {
-    Ok(0)
+fn part2(input: &Input) -> Result<u64> {
+    let map = parse_input(input);
+    let result = steps_sim(&map);
+    Ok(result)
 }
 
 #[cfg(test)]
@@ -94,11 +128,17 @@ mod test {
         Ok(())
     }
 
-    // #[test]
-    // pub fn test_part2() -> Result<()> {
-    //     let input = Input::from_lines([
-    //     ]);
-    //     assert_eq!(part2(&input).unwrap(), 0);
-    //     Ok(())
-    // }
+    #[test]
+    pub fn test_part2() -> Result<()> {
+        let input = Input::load("example3")?;
+        assert_eq!(part2(&input).unwrap(), 6);
+        Ok(())
+    }
+
+    #[test]
+    pub fn test_part2_input() -> Result<()> {
+        let input = Input::load("input")?;
+        assert_eq!(part2(&input).unwrap(), 14935034899483u64);
+        Ok(())
+    }
 }
